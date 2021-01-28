dbac7b48ebc0e9f9bcd20b53ccbdc90268f3907b
Shambav <sambhav.s@hcl.com>
2021-01-28 23:40:23 +0530

    Added New Project

---
+++ CustomerManagementAPI/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+using Serilog;
+using System.IO;
+
+using Microsoft.Extensions.Configuration;
+
+
+
+
+
+namespace Pitstop.CustomerManagementAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((buildercontext, config) =>
+                {
+                    config.AddJsonFile("settings/appsettings_customersecret.json", optional: true);
+                })
+                .UseSerilog()
+                .UseHealthChecks("/hc")
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
